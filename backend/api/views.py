55df21b806bfa92287cf94acc3e1f498050dc435
neeraj-395 <nsinghh.04@gmail.com>
2025-03-15 11:43:57 +0530

    idk

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from api.models import *
 from django.contrib.contenttypes.models import ContentType
 from rest_framework import status
